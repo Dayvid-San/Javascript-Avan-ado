a3a8e574d6236111d3d4a6470618f7731d1a2565
Dayvid Santana <santana.dayvid@outlook.com>
2022-03-15 17:33:40 -0300

    finalizando ideias para herança

--- heranca.js
+++ heranca.js
@@ -1,46 +1,39 @@
 class Dev {
-    constructor(nome, idade, mainProgrLang){
+    constructor(nome, idade, mainProgrLang, tipoDev){
         this.nome = nome;
         this.idade = idade;
         this.mainProgrLang = mainProgrLang;
+        this.tipoDev = tipoDev
     }
 
     saudacao(){
-        console.log(`Olá! Sou um Dev. Trabalho principalmente com ${this.mainProgrLang} e me chamo ${this.nome}\n\n`)
+        console.log(`Olá! Sou um Dev ${this.tipoDev}. Trabalho principalmente com ${this.mainProgrLang} e me chamo ${this.nome}\n\n`)
     }
 }
 
 
 class Frontend extends Dev {
-    constructor(nome, idade, mainProgrLang, framework){
-        super()
-        this.nome = nome;
-        this.idade = idade;
-        this.mainProgrLang = mainProgrLang;
+    constructor(nome, idade, mainProgrLang, tipoDev ,framework){
+        super(nome, idade, mainProgrLang, tipoDev)
         this.framework = framework
     }
 
-    saudacao(){
-        console.log(`Olá! Meu nome é ${this.nome}, tenho ${this.idade}. Sou um dev Front-End. Trabalho principalmente com ${this.mainProgrLang} e meu principal framework é o${this.framework}\n\n`)
-    }
+    
 }
 
 class Backend extends Dev {
-    constructor(nome, idade, mainProgrLang, bancodedados){
-        super()
-        this.nome = nome;
-        this.idade = idade;
-        this.mainProgrLang = mainProgrLang;
+    constructor(nome, idade, mainProgrLang, tipoDev ,bancodedados){
+        super(nome, idade, mainProgrLang, tipoDev)
         this.bancodedados = bancodedados;
     }
 
-    saudacao(){
-        console.log(`Olá! Meu nome é ${this.nome}, tenho ${this.idade}. Sou um dev Back-End. Trabalho principalmente com ${this.mainProgrLang} e meu principal banco de dados é o ${this.bancodedados}\n\n`)
-    }
+   
 }
 
-const dayvid = new Backend('Dayvid', 21, 'Javascript', 'MongoDB')
-const diogo = new Frontend('Diogo', 23, 'Javascript', 'ReactJS')
+const dayvid = new Backend('Dayvid', 21, 'Javascript', 'Back-end', 'MongoDB')
+const diogo = new Frontend('Diogo', 23, 'Javascript', 'Front-end', 'ReactJS')
 
+console.log(dayvid)
 dayvid.saudacao()
+console.log(diogo)
 diogo.saudacao()
